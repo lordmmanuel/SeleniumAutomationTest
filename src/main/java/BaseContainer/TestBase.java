550a86221a4f3a12ffdaed504d49b2eca5bcb470
Adekoya <Gbolahan.Adekoya@CORALPAY.LOCAL>
2024-05-27 17:38:21 +0100

    Setup Configuration and Locators Files

--- src/main/java/BaseContainer/TestBase.java
+++ src/main/java/BaseContainer/TestBase.java
@@ -1,5 +1,10 @@
 package BaseContainer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,14 +14,52 @@ import org.testng.annotations.BeforeTest;
 
 public class TestBase {
 	public WebDriver driver;
+	public Properties config = new Properties();
+	public Properties locator = new Properties();
+	public FileInputStream file;
+	String ProjectLocation = System.getProperty("user.dir");
+	
 	
 	@BeforeTest
-	public void Setup() throws InterruptedException {		
+	public void Setup() throws InterruptedException {	
+		
+		//Launching/Setup Browser
 		ChromeOptions options = new ChromeOptions();
 		driver = new ChromeDriver(options);
-		driver.get("https://demo.seleniumeasy.com/");
+		
+		//URL & Data File Configuration Setup
+		try {
+			file = new FileInputStream(ProjectLocation + "\\src\\main\\resources\\PropertyFiles\\config.properties");
+		}catch(FileNotFoundException FNFE) {
+			FNFE.printStackTrace();
+		}
+		
+		//Loading the Configured File
+		try {
+			config.load(file);
+		}catch(IOException FNIO) {
+			FNIO.printStackTrace();
+		}
+		
+		//Locators File Configuration
+		try {
+			file = new FileInputStream(ProjectLocation + "\\src\\main\\resources\\PropertyFiles\\locator.properties");
+		}catch(FileNotFoundException FNFE) {
+			FNFE.printStackTrace();
+		}
+		
+		//Loading the Configured File
+		try {
+			locator.load(file);
+		}catch(IOException FNIO) {
+			FNIO.printStackTrace();
+		}
+		
+		
+		//Application Launching
+		driver.get(config.getProperty("url"));
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//a[@id='btn_basic_example']")).click();
+		driver.findElement(By.xpath(locator.getProperty("StartPractising"))).click();
 		Thread.sleep(1000);
 	}
 	
--- src/main/java/Tests/LoginTest.java
+++ src/main/java/Tests/LoginTest.java
@@ -9,40 +9,40 @@ public class LoginTest extends TestBase {
 	
 	@Test(priority = 0)
 	public void SingleCheckBox() {			
-		driver.findElement(By.xpath("//a[contains(text(),'Check Box Demo')]")).click();
-		driver.findElement(By.xpath("//body/div[@id='easycont']/div[1]/div[2]/div[1]/div[2]/div[1]/form[1]/label[2]/input[1]")).click();
+		driver.findElement(By.xpath(locator.getProperty("CheckBoxDemo"))).click();
+		driver.findElement(By.xpath(locator.getProperty("SinDefaultUncheck"))).click();
 	}
 			
 	@Test(priority = 1)
 	public void SelFirstOpt() {			
-		driver.findElement(By.xpath("//input[@id='isAgeSelected']")).click();
-		driver.findElement(By.xpath("//body[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/label[1]")).click();
+		driver.findElement(By.xpath(locator.getProperty("CheckBoxSelected"))).click();
+		driver.findElement(By.xpath(locator.getProperty("SelectFirstOpt"))).click();
 	}
 	
 	@Test(priority = 2)
 	public void SelSecondOpt() {
-		driver.findElement(By.xpath("//body/div[@id='easycont']/div[1]/div[2]/div[2]/div[2]/div[2]/label[1]/input[1]")).click();
+		driver.findElement(By.xpath(locator.getProperty("SelectSecondOpt"))).click();
 	}
 	
 	@Test(priority = 3)
 	public void SelThirdOpt() {
-		driver.findElement(By.xpath("//body/div[@id='easycont']/div[1]/div[2]/div[2]/div[2]/div[3]/label[1]/input[1]")).click();
+		driver.findElement(By.xpath(locator.getProperty("SelectThirdOpt"))).click();
 	}
 	
 	@Test(priority = 4)
 	public void SelFourthOpt() {
-		driver.findElement(By.xpath("//body/div[@id='easycont']/div[1]/div[2]/div[2]/div[2]/div[4]/label[1]/input[1]")).click();
+		driver.findElement(By.xpath(locator.getProperty("SelectFourthOpt"))).click();
 	}
 	
 	@Test(priority = 5)
 	public void UncheckAllOpt() throws InterruptedException {
-		driver.findElement(By.cssSelector("#check1")).click();
+		driver.findElement(By.cssSelector(locator.getProperty("UncheckAll"))).click();
 		Thread.sleep(1000);
 	}
 	
 	@Test(priority = 6)
 	public void CheckAllOpt() {
-		driver.findElement(By.xpath("//input[@id='check1']")).click();
+		driver.findElement(By.xpath(locator.getProperty("CheckAll"))).click();
 	}			
 	
 
